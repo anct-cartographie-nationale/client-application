990769a6872f6446de05376ee75aa9959bce6e5b
Hugo SUBTIL <ext.sopra.husubtil@grandlyon.com>
2020-10-02 12:01:05 +0200

    feat(structure-list): update layout

--- src/app/structure-list/structure-list.component.ts
+++ src/app/structure-list/structure-list.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-structure-list',
   templateUrl: './structure-list.component.html',
-  styleUrls: ['./structure-list.component.scss']
+  styleUrls: ['./structure-list.component.scss'],
 })
 export class StructureListComponent implements OnInit {
+  public regisrationFormLink: string = environment.registrationForm;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
