4dd91784c1d12a3866de6d54b71c5b5e874c0e58
Jeremie BRISON <ext.sopra.jbrison@grandlyon.com>
2020-10-12 15:40:03 +0200

    fix(search) : Fix btn design clicked + init modal
    Utilisation du TS plutôt que des propriétés CSS pour la gestion Activé/Désactivé des filtres

--- src/app/structure/components/recherche/recherche.component.ts
+++ src/app/structure/components/recherche/recherche.component.ts
@@ -3,13 +3,42 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-recherche',
   templateUrl: './recherche.component.html',
-  styleUrls: ['./recherche.component.scss']
+  styleUrls: ['./recherche.component.scss'],
 })
 export class RechercheComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {}
+  modalOpened: string;
+  modalType: string[] = ['services', 'accueil', 'plusFiltres'];
+  btnServicesChecked: boolean;
+  btnHomeChecked: boolean;
+  btnMoreFilterchecked: boolean;
   ngOnInit(): void {
+    this.modalOpened = null;
+    this.btnServicesChecked = false;
   }
 
+  openModal(option: string) {
+    this.modalOpened = null;
+    switch (option) {
+      case this.modalType[0]:
+        this.btnServicesChecked = !this.btnServicesChecked;
+        this.btnHomeChecked = false;
+        this.btnMoreFilterchecked = false;
+        if (this.btnServicesChecked) this.modalOpened = this.modalType[0];
+
+        break;
+      case this.modalType[1]:
+        this.btnHomeChecked = !this.btnHomeChecked;
+        this.btnMoreFilterchecked = false;
+        this.btnServicesChecked = false;
+        if (this.btnHomeChecked) this.modalOpened = this.modalType[1];
+        break;
+      case this.modalType[2]:
+        this.btnMoreFilterchecked = !this.btnMoreFilterchecked;
+        this.btnServicesChecked = false;
+        this.btnHomeChecked = false;
+        if (this.btnMoreFilterchecked) this.modalOpened = this.modalType[2];
+        break;
+    }
+  }
 }
