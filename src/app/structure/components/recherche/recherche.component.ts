8120b0a3ba74c06e8d9008b56c85fbefe05ff2af
Jeremie BRISON <ext.sopra.jbrison@grandlyon.com>
2020-10-13 17:08:25 +0200

    fix(checkbox) : fix checkbox + clean/maj code
    Etat stable des checkbox multiples dans les modals

--- src/app/structure/components/recherche/recherche.component.ts
+++ src/app/structure/components/recherche/recherche.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Service } from '../../models/recherche.model';
-import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
+import { Module } from '../../models/recherche.model';
 
 @Component({
   selector: 'app-recherche',
@@ -8,99 +7,134 @@ import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
   styleUrls: ['./recherche.component.scss'],
 })
 export class RechercheComponent implements OnInit {
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      checkArray: this.fb.array([]),
-    });
-  }
-  form: FormGroup;
-  modalOpened: string;
+  constructor() {}
+  //Variables btn filtres
   modalType: string[] = ['services', 'accueil', 'plusFiltres'];
-  btnServicesChecked: boolean;
-  btnHomeChecked: boolean;
-  btnMoreFilterchecked: boolean;
-  test = '50%';
-  services: Service[];
+
+  //Variable gestion liste modal
+  servicesCategories: Module[];
+  modaliteCategories: Module[];
+  modules: Module[];
+  filtresCategories: Module[];
+  modalOpened: string;
+
+  //Variable gestion Checkbox
+  checkedTab: string[];
+  filterCheck: string[];
 
   ngOnInit(): void {
+    //Sert à afficher la modal et indiquer le type de filtre choisit
     this.modalOpened = null;
-    this.btnServicesChecked = false;
-    this.services = [];
-    //Block en attendant
-    this.mockService('Accompagnement aux démarches en ligne', 'CAF', 7);
-    this.mockService('Insertion sociale et professionnelle', ' Diffuser son CV en ligne', 5);
-    this.mockService('Accès aux droits', 'Déclarer ses revenus en ligne et découvertes des services proposés', 8);
-    this.mockService('Aide à la parentalité/éducation', 'Découvrir l’univers des jeux vidéos', 4);
-    this.mockService('Compétences de base', 'Faire un diagnostic des compétences', 8);
-    this.mockService('Culture et sécurité numérique', 'Traitement de texte : découverte', 4);
-    //Fin block en attendant
-  }
-  //Fonction en attendant
-  mockService(titre: string, categ: string, nbCateg: number) {
-    var service1 = new Service();
-    service1.titre = titre;
-    service1.categories = [];
-    for (var i = 0; i < nbCateg; i++) {
-      service1.categories.push(categ + '_' + i);
-    }
-    this.services.push(service1);
-  }
-  //Fin fonction en attendant
-  openModal(option: string) {
-    this.modalOpened = null;
-    switch (option) {
-      case this.modalType[0]:
-        this.btnServicesChecked = !this.btnServicesChecked;
-        this.btnHomeChecked = false;
-        this.btnMoreFilterchecked = false;
-        if (this.btnServicesChecked) this.modalOpened = this.modalType[0];
-        break;
-      case this.modalType[1]:
-        this.btnHomeChecked = !this.btnHomeChecked;
-        this.btnMoreFilterchecked = false;
-        this.btnServicesChecked = false;
-        if (this.btnHomeChecked) this.modalOpened = this.modalType[1];
-        break;
-      case this.modalType[2]:
-        this.btnMoreFilterchecked = !this.btnMoreFilterchecked;
-        this.btnServicesChecked = false;
-        this.btnHomeChecked = false;
-        if (this.btnMoreFilterchecked) this.modalOpened = this.modalType[2];
-        break;
-    }
-  }
-  applyFilter() {
-    this.openModal(this.modalOpened);
-    console.log(this.form.value);
+
+    //Sert à stocker les différentes catégories
+    this.servicesCategories = [];
+    this.modaliteCategories = [];
+    this.modules = [];
+    this.filtresCategories = [];
+
+    //Sert à gérer la checkbox multiple
+    this.checkedTab = new Array();
+    this.filterCheck = new Array();
   }
 
+  //Ouvrir la modal et afficher la liste en fonction du btn de filtre appuyé
+  openModal(option: string) {
+    this.modules = [];
+    switch (option) {
+      case this.modalType[0]:
+        //Vérifie si le btn n'est pas actif
+        if (this.modalOpened != this.modalType[0]) {
+          this.modalOpened = this.modalType[0];
+          this.fakeDataServices();
+        } else {
+          this.modalOpened = null;
+        }
+        break;
+      case this.modalType[1]:
+        //Vérifie si le btn n'est pas actif
+        if (this.modalOpened != this.modalType[1]) {
+          this.modalOpened = this.modalType[1];
+          this.fakeDataModalite();
+        } else {
+          this.modalOpened = null;
+        }
+        break;
+      case this.modalType[2]:
+        //Vérifie si le btn n'est pas actif
+        if (this.modalOpened != this.modalType[2]) {
+          this.modalOpened = this.modalType[2];
+          this.fakeDataFiltres();
+        } else {
+          this.modalOpened = null;
+        }
+        break;
+    }
+    //Initialisation de la liste temporaire
+    this.checkedTab = this.filterCheck.slice();
+  }
+
+  //Envoie d'un tableau contenant tous les filtres
+  applyFilter() {
+    this.filterCheck = this.checkedTab.slice();
+    this.openModal(this.modalOpened);
+    console.log(this.filterCheck);
+  }
+
+  //Gestion de l'evenement checkbox(Cocher/Décocher)
   onCheckboxChange(e, reset: boolean) {
-    const checkArray: FormArray = this.form.get('checkArray') as FormArray;
-    if (reset) {
+    //Condition btn effacer filtre d'une liste
+    if (!reset) {
       if (e.target.checked) {
-        checkArray.push(new FormControl(e.target.value));
+        this.checkedTab.push(e.target.value);
       } else {
-        let i: number = 0;
-        checkArray.controls.forEach((item: FormControl) => {
-          if (item.value == e.target.value) {
-            checkArray.removeAt(i);
-            return;
-          }
-          i++;
-        });
+        //Vérifie si la case décochée est présente dans la liste temporaire et la supprime
+        if (this.checkedTab.indexOf(e.target.value) > -1) {
+          this.checkedTab.splice(this.checkedTab.indexOf(e.target.value), 1);
+        }
       }
     } else {
-      checkArray.clear();
+      //Efface uniquement les éléments de la liste en cours
+      this.modules.forEach((m) => {
+        m.categories.forEach((categ) => {
+          if (this.checkedTab.indexOf(categ) > -1) this.checkedTab.splice(this.checkedTab.indexOf(categ), 1);
+        });
+      });
     }
   }
-  isChecked(module: string) {
-    const checkArray: FormArray = this.form.get('checkArray') as FormArray;
-    var bool: boolean = false;
-    checkArray.controls.forEach((item: FormControl) => {
-      if (item.value == module) {
-        bool = true;
-      }
-    });
-    return bool;
+
+  /**
+   * En attendant les apis
+   */
+  mockService(module: Module[], titre: string, categ: string, nbCateg: number) {
+    var m = new Module();
+    m.titre = titre;
+    m.categories = [];
+    for (var i = 0; i < nbCateg; i++) {
+      m.categories.push(categ + i);
+    }
+    module.push(m);
+  }
+  fakeDataServices() {
+    this.mockService(this.modules, 'Accompagnement aux démarches en ligne', 'CAF', 7);
+    this.mockService(this.modules, 'Insertion sociale et professionnelle', ' Diffuser son CV en ligne', 5);
+    this.mockService(
+      this.modules,
+      'Accès aux droits',
+      'Déclarer ses revenus en ligne et découvertes des services proposés',
+      8
+    );
+    this.mockService(this.modules, 'Aide à la parentalité/éducation', 'Découvrir l’univers des jeux vidéos', 4);
+    this.mockService(this.modules, 'Compétences de base', 'Faire un diagnostic des compétences', 8);
+    this.mockService(this.modules, 'Culture et sécurité numérique', 'Traitement de texte : découverte', 4);
+  }
+  fakeDataModalite() {
+    this.mockService(this.modules, "Modalité d'accueil", 'Matériel mis à dispostion', 6);
+  }
+  fakeDataFiltres() {
+    this.mockService(this.modules, 'Équipements', 'Accès à des revues ou livres infoirmatiques numériques', 8);
+    this.mockService(this.modules, "Type d'acteurs", 'Lieux de médiation (Pimms, assos...)', 5);
+    this.mockService(this.modules, 'Publics', 'Langues étrangères autres qu’anglais', 12);
+    this.mockService(this.modules, 'Labelisation', 'Prescripteur du Pass Numérique', 6);
+    this.mockService(this.modules, 'Type de structure', 'Espace de co-working', 6);
   }
 }
--- src/app/structure/models/recherche.model.ts
+++ src/app/structure/models/recherche.model.ts
@@ -1,4 +1,4 @@
-export class Service {
+export class Module {
   titre: string;
   categories: string[];
   size: number;
