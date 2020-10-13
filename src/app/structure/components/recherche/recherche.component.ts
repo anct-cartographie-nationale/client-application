2f26a9680a3df06fd520bcd73c84b0d703890d33
Jeremie BRISON <ext.sopra.jbrison@grandlyon.com>
2020-10-13 09:14:28 +0200

    fix(search) : add modal + module list

--- src/app/structure/components/recherche/recherche.component.ts
+++ src/app/structure/components/recherche/recherche.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Service } from '../../models/recherche.model';
 
 @Component({
   selector: 'app-recherche',
@@ -12,11 +13,32 @@ export class RechercheComponent implements OnInit {
   btnServicesChecked: boolean;
   btnHomeChecked: boolean;
   btnMoreFilterchecked: boolean;
+  test = '50%';
+  services: Service[];
+
   ngOnInit(): void {
-    this.modalOpened = null;
+    this.modalOpened = 'services';
     this.btnServicesChecked = false;
+    this.services = [];
+    this.mockService('Accompagnement aux démarches en ligne', 'CAF', 7);
+    this.mockService('Insertion sociale et professionnelle', 'Diffuser son CV en ligne', 5);
+    this.mockService('Accès aux droits', 'Déclarer ses revenus en ligne et découvertes des services proposés', 8);
+    this.mockService('Aide à la parentalité/éducation', 'Découvrir l’univers des jeux vidéos', 4);
+    this.mockService('Compétences de base', 'Faire un diagnostic des compétences', 8);
+    this.mockService('Culture et sécurité numérique', 'Traitement de texte : découverte', 4);
+    console.log(this.services);
+    this.calcSizeCol(this.services);
   }
 
+  mockService(titre: string, categ: string, nbCateg: number) {
+    var service1 = new Service();
+    service1.titre = titre;
+    service1.categories = [];
+    for (var i = 0; i < nbCateg; i++) {
+      service1.categories.push(categ + '_' + i);
+    }
+    this.services.push(service1);
+  }
   openModal(option: string) {
     this.modalOpened = null;
     switch (option) {
@@ -25,7 +47,6 @@ export class RechercheComponent implements OnInit {
         this.btnHomeChecked = false;
         this.btnMoreFilterchecked = false;
         if (this.btnServicesChecked) this.modalOpened = this.modalType[0];
-
         break;
       case this.modalType[1]:
         this.btnHomeChecked = !this.btnHomeChecked;
@@ -39,6 +60,26 @@ export class RechercheComponent implements OnInit {
         this.btnHomeChecked = false;
         if (this.btnMoreFilterchecked) this.modalOpened = this.modalType[2];
         break;
+    }
+  }
+  applyFilter() {
+    this.openModal(this.modalOpened);
+  }
+
+  calcSizeCol(services: Service[]) {
+    for (var i = 0; i < services.length; i++) {
+      if (services[i + 1]) {
+        var nb1, nb2: number;
+        nb1 = services[i].categories.length;
+        nb2 = services[i + 1].categories.length;
+        if (nb1 + nb2 <= 13) {
+          services[i].size = parseFloat((nb1 / 13).toPrecision(2)) * 100;
+          services[i + 1].size = parseFloat((1 - services[i].size / 100).toPrecision(2)) * 100;
+          i++;
+        } else {
+          services[i].size = 100;
+        }
+      }
     }
   }
 }
---
+++ src/app/structure/models/recherche.model.ts
@@ -0,0 +1,5 @@
+export class Service {
+  titre: string;
+  categories: string[];
+  size: number;
+}
