2c6c7957ce734c397d772f8b1fd36afc6a7975d6
Jérémie BRISON <ext.sopra.jbrison@grandlyon.com>
2020-10-15 15:23:19 +0200

    feat(topic-card) : add structure card feature for search display

--- src/app/structure-list/structure-list.component.ts
+++ src/app/structure-list/structure-list.component.ts
@@ -1,14 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-structure-list',
   templateUrl: './structure-list.component.html',
-  styleUrls: ['./structure-list.component.scss'],
 })
 export class StructureListComponent implements OnInit {
-  public regisrationFormLink: string = environment.registrationForm;
-
   constructor() {}
 
   ngOnInit(): void {}
