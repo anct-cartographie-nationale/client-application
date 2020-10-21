49defde1d759b866cd5ddff5507e43cef6f200c7
Jeremie BRISON <ext.sopra.jbrison@grandlyon.com>
2020-10-21 10:59:40 +0200

    fix(search) : add some test + fix design modal

--- src/app/structure-list/components/search/search.component.spec.ts
+++ src/app/structure-list/components/search/search.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { Category } from '../../models/category.model';
+import { Filter } from '../../models/filter.model';
+import { Module } from '../../models/module.model';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SearchComponent } from './search.component';
+import { SearchService } from '../../services/search.service';
 
-describe('RechercheComponent', () => {
+describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SearchComponent],
+      imports: [HttpClientTestingModule, ReactiveFormsModule],
     }).compileComponents();
   });
 
@@ -20,5 +26,23 @@ describe('RechercheComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit filters', () => {
+    const filter: Filter[] = [new Filter('nom', 'valInput', false)];
+    spyOn(component.searchEvent, 'emit');
+    component.applyFilter('valInput');
+    expect(component.searchEvent.emit).toHaveBeenCalled();
+    expect(component.searchEvent.emit).toHaveBeenCalledWith(filter);
+  });
+
+  it('should update categories', () => {
+    let categories: Category[] = [new Category({ name: 'Accompagnement des démarches' })];
+    categories[0].modules = [];
+    for (let i = 0; i < 7; i++) {
+      categories[0].modules.push(new Module(5 + i, 'CAF' + i));
+    }
+    component.openModal('accompagnement');
+    expect(component.categories).toEqual(categories);
   });
 });
--- src/app/structure-list/services/search.service.spec.ts
+++ src/app/structure-list/services/search.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SearchService } from './search.service';
 
 describe('SearchService', () => {
   let service: SearchService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(SearchService);
   });
 
