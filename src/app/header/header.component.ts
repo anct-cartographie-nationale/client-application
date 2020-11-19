9ff17fa9a5ccadc90709b31a6a12b62a1e01e77a
Hugo SUBTIL <ext.sopra.husubtil@grandlyon.com>
2020-11-19 15:11:49 +0100

    fix: clean header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +6,7 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.route.queryParams.subscribe((params) => {
-      console.log(params);
-    });
-  }
+  ngOnInit(): void {}
 }
