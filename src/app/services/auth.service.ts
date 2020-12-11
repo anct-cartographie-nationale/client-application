337a90ce3b84088f5ff244229f94e3836be92e8e
Hugo SUBTIL <ext.sopra.husubtil@grandlyon.com>
2020-12-11 12:13:42 +0100

    fix: switch method from public to private

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -37,7 +37,7 @@ export class AuthService {
     return false;
   }
 
-  public getExpiration(): DateTime {
+  private getExpiration(): DateTime {
     return DateTime.fromISO(this.userValue.expiresAt, { zone: 'Europe/Paris' });
   }
 
