aad1d388e954ceaf05989777a206f00f0012d3a1
Jeremie BRISON <ext.sopra.jbrison@grandlyon.com>
2020-12-21 10:34:32 +0100

    fix(dev) : fix height + width for EPN demarche

--- src/app/shared/enum/demarches.enum.ts
+++ src/app/shared/enum/demarches.enum.ts
@@ -2,7 +2,7 @@ export enum Demarches {
   accompagnantCaf = 'Caf',
   carsat = 'Carsat',
   cpam = 'Cpam',
-  espacePublicNumeriqueepn = 'Espace public Numérique (EPN)',
+  espacePublicNumeriqueepn = 'Espace public Numérique',
   impots = 'Impôts',
   logement = 'Logements',
   poleEmploi = 'Pôle Emploi',
