b5c264b807e92800550ac8a5bebaa649be80771f
Hugo SUBTIL <ext.sopra.husubtil@grandlyon.com>
2021-01-15 18:32:29 +0100

    feat: add address search with auto-complete

---
+++ src/app/shared/components/address-autocomplete/address-autocomplete.component.ts
@@ -0,0 +1,35 @@
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { AddressService } from '../../service/address.service';
+
+@Component({
+  selector: 'app-address-autocomplete',
+  templateUrl: './address-autocomplete.component.html',
+  styleUrls: ['./address-autocomplete.component.scss'],
+})
+export class AddressAutocompleteComponent {
+  public data = [];
+  @ViewChild('searchAddress', { static: true }) searchAddress: ElementRef;
+  @Output() selectedAddress: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor(private addressService: AddressService) {}
+
+  public onSearchChange(searchString: string) {
+    this.addressService.searchAddress(searchString).subscribe((data) => {
+      this.data = data.hits.hits;
+    });
+  }
+
+  public selectedResult(hit: any): void {
+    const value = this.parseHitToAddress(hit);
+    // Set input value
+    this.searchAddress.nativeElement.value = value;
+    // Reset autocomplete
+    this.data = [];
+    // Emit choosen value
+    this.selectedAddress.emit(value);
+  }
+
+  public parseHitToAddress(hit: any): string {
+    return `${hit._source['data-fr'].properties.numero_str} ${hit._source['data-fr'].properties.voie_str} ${hit._source['data-fr'].properties.commune_str}`;
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,9 +7,10 @@ import { SharedComponents } from './components';
 import { SharedPipes } from './pipes';
 import { SharedDirectives } from './directives';
 import { SvgIconComponent } from './components/svg-icon/svg-icon.component';
+import { AddressAutocompleteComponent } from './components/address-autocomplete/address-autocomplete.component';
 @NgModule({
   imports: [CommonModule, RouterModule, FlexLayoutModule, ReactiveFormsModule],
-  declarations: [...SharedPipes, ...SharedComponents, ...SharedDirectives, SvgIconComponent],
+  declarations: [...SharedPipes, ...SharedComponents, ...SharedDirectives, SvgIconComponent, AddressAutocompleteComponent],
   exports: [
     ...SharedPipes,
     ...SharedComponents,
