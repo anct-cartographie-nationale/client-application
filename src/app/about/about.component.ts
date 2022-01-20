3c5f554b0a00de8e2dfef0cb76b76ed30d090036
Marlène SIMONDANT <msimondant@grandlyon.com>
2022-01-20 09:24:14 +0000

    feat(version): Display app version

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { version } from '../../../package.json';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  public version: string = version;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
