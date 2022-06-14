dbc353b42e896e99aa3b45f5d8739681bb576ec5
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2022-06-14 13:01:42 +0200

    fix: remove not necessary class

--- src/root/layouts/main/main.layout.ts
+++ src/root/layouts/main/main.layout.ts
@@ -4,9 +4,4 @@ import { Router } from '@angular/router';
 @Component({
   templateUrl: './main.layout.html'
 })
-export class MainLayout {
-  currentRoute?: string = '';
-  constructor(private router: Router) {
-    this.currentRoute = router.url;
-  }
-}
+export class MainLayout {}
