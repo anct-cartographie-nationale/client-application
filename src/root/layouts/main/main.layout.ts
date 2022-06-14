a9eb4dec3cc0aa857e2fe69c47909171be642dae
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2022-06-14 10:29:08 +0200

    feat: add style for navigation links

--- src/root/layouts/main/main.layout.ts
+++ src/root/layouts/main/main.layout.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  templateUrl: './main.layout.html',
-  styleUrls: ['./main.layout.css']
+  templateUrl: './main.layout.html'
 })
-export class MainLayout {}
+export class MainLayout {
+  currentRoute?: string = '';
+  constructor(private router: Router) {
+    this.currentRoute = router.url;
+  }
+}
