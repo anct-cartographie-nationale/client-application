c59baa872dd7db615c99b152068350ca9708caa0
Arthur Belkhayat <arthur.belkhayat@gmail.com>
2022-06-15 19:56:14 +0200

    feat: add collapse component (#18)

--- src/features/orientation/infrastructure/presentation/modules/orientation.feature.module.ts
+++ src/features/orientation/infrastructure/presentation/modules/orientation.feature.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrientationLayout } from '../layouts';
+import { CollapseComponent } from '../components/collapse';
 import { OrientationFeatureRoutingModule } from './orientation.feature-routing.module';
 import { BesoinPage, DatePage, DemarrerPage, LocalisationPage, SpecificitePage } from '../pages';
 
 @NgModule({
-  declarations: [OrientationLayout, BesoinPage, DatePage, DemarrerPage, LocalisationPage, SpecificitePage],
+  declarations: [OrientationLayout, BesoinPage, DatePage, DemarrerPage, LocalisationPage, SpecificitePage, CollapseComponent],
   imports: [OrientationFeatureRoutingModule, CommonModule]
 })
 export class OrientationFeatureModule {}
