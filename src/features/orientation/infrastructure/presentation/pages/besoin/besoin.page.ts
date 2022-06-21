78293b341bd7c13925ba2dc196f4462b26945e0e
Arthur Belkhayat <arthur.belkhayat@gmail.com>
2022-06-21 11:30:24 +0200

    feat: besoin pages (#22)
    * feat: add besoin page
    
    * fix: adjust some styles
    
    * fix: revert attribut

--- src/features/orientation/infrastructure/presentation/pages/besoin/besoin.page.ts
+++ src/features/orientation/infrastructure/presentation/pages/besoin/besoin.page.ts
@@ -4,4 +4,17 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: 'besoin.page.html'
 })
-export class BesoinPage {}
+export class BesoinPage {
+  public demarche: string[][] = [
+    ['Accès aux droits', 'Logement', 'Accompagnement CAF', 'Insertion Pro'],
+    ['Assistance et coups de pouce', 'Autres démarches']
+  ];
+  public savoir: string[][] = [
+    ['Diagnostic', 'Compétences de base', 'Culture numérique'],
+    ['Formation', 'Outils de bureautique', 'Outils créatifs', 'Aide à la parentalité']
+  ];
+  public equipement: string[][] = [
+    ['Wifi', 'Ordinateur', 'Imprimante', 'Scanner'],
+    ['Prêt / Don matériel', 'Revues informatiques', 'FabLab']
+  ];
+}
