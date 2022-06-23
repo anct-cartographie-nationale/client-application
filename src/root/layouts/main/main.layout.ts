a331a57b585c20e2ba8f1538bb51451497f7ed2a
Marc Gavanier <marc.gavanier@gmail.com>
2022-06-23 17:52:58 +0200

    feat: add brand name and logo in main layout navbar

--- src/root/layouts/main/main.layout.ts
+++ src/root/layouts/main/main.layout.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { BRAND_TOKEN, BrandConfiguration } from '../../configuration';
 
 @Component({
   templateUrl: './main.layout.html'
 })
-export class MainLayout {}
+export class MainLayout {
+  public constructor(@Inject(BRAND_TOKEN) public readonly brandConfiguration: BrandConfiguration) {}
+}
