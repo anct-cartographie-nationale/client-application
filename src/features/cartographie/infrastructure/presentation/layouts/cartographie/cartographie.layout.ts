fa3b8969f44d75133435d504d9c6497a9d64a563
Marc Gavanier <marc.gavanier@gmail.com>
2022-06-28 09:24:02 +0200

    refactor: create a presenter for cartographie layout

--- src/features/cartographie/infrastructure/presentation/layouts/cartographie/cartographie.layout.ts
+++ src/features/cartographie/infrastructure/presentation/layouts/cartographie/cartographie.layout.ts
@@ -3,16 +3,25 @@ import { Structure } from '@gouvfr-anct/mediation-numerique';
 import { Router } from '@angular/router';
 import { delay, Observable } from 'rxjs';
 import { GeolocationPresenter, LieuxMediationNumeriqueRepository, MarkersPresenter } from '../../../../domain';
+import { CartographiePresenter } from '../../../../domain/presenters/cartographie/cartographie.presenter';
 
 @Component({
   templateUrl: './cartographie.layout.html',
-  providers: [GeolocationPresenter, MarkersPresenter]
+  providers: [
+    GeolocationPresenter,
+    {
+      deps: [LieuxMediationNumeriqueRepository],
+      provide: CartographiePresenter,
+      useClass: CartographiePresenter
+    },
+    MarkersPresenter
+  ]
 })
 export class CartographieLayout {
-  public structures$: Observable<Structure[]> = this.lieuxMediationNumeriqueRepository.getAll$().pipe(delay(0));
+  public structures$: Observable<Structure[]> = this.cartographiePresenter.getStructures$().pipe(delay(0));
 
   public constructor(
-    private readonly lieuxMediationNumeriqueRepository: LieuxMediationNumeriqueRepository,
+    private readonly cartographiePresenter: CartographiePresenter,
     public readonly markersPresenter: MarkersPresenter,
     private readonly router: Router
   ) {}
---
+++ src/features/cartographie/infrastructure/presentation/models/structure.ts
@@ -0,0 +1,24 @@
+import { LieuMediationNumerique } from '../../../../../models/lieu-mediation-numerique/lieu-mediation-numerique';
+import { Structure } from '@gouvfr-anct/mediation-numerique';
+import { ifAny } from '../../utilities/if-any';
+
+export const toStructurePresentation = (lieuMediationNumerique: LieuMediationNumerique): Structure =>
+  new Structure({
+    _id: lieuMediationNumerique.id,
+    structureName: lieuMediationNumerique.nom,
+    address: {
+      street: lieuMediationNumerique.adresse.voie,
+      postcode: lieuMediationNumerique.adresse.code_postal,
+      commune: lieuMediationNumerique.adresse.commune,
+      coordinates: [lieuMediationNumerique.localisation.longitude, lieuMediationNumerique.localisation.latitude]
+    },
+    coord: [lieuMediationNumerique.localisation.longitude, lieuMediationNumerique.localisation.latitude],
+    ...ifAny('contactPhone', lieuMediationNumerique.contact?.telephone),
+    ...ifAny('contactMail', lieuMediationNumerique.contact?.courriel),
+    ...ifAny('website', lieuMediationNumerique.contact?.site_web),
+    ...ifAny('labelsQualifications', lieuMediationNumerique.labels_nationaux),
+    ...ifAny('updatedAt', lieuMediationNumerique.date_maj, (date: Date) => date.toISOString())
+  });
+
+export const toStructuresPresentation = (lieuxMediationNumerique: LieuMediationNumerique[]): Structure[] =>
+  lieuxMediationNumerique.map(toStructurePresentation);
