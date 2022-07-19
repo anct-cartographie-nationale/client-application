0920ce85044225f97b5b65155aaf8f132f7d1f88
Marc Gavanier <marc.gavanier@gmail.com>
2022-07-19 13:08:41 +0200

    fix: update liste lieux mediation numerique style (#45)

--- src/features/cartographie/infrastructure/presentation/components/lieu-mediation-numerique-list-item/lieu-mediation-numerique-list-item.component.ts
+++ src/features/cartographie/infrastructure/presentation/components/lieu-mediation-numerique-list-item/lieu-mediation-numerique-list-item.component.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { LieuMediationNumeriqueListItemPresentation } from '@features/cartographie/domain/presenters/lieux-mediation-numerique-list/lieu-mediation-numerique-list-item.presentation';
-import { LieuMediationNumerique } from '../../../../../../models/lieu-mediation-numerique/lieu-mediation-numerique';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -9,6 +8,4 @@ import { LieuMediationNumerique } from '../../../../../../models/lieu-mediation-
 })
 export class LieuMediationNumeriqueListItemComponent {
   @Input() public lieuMediationNumerique!: LieuMediationNumeriqueListItemPresentation;
-  @Output() public showDetails: EventEmitter<LieuMediationNumerique> = new EventEmitter<LieuMediationNumerique>();
-  @Output() public hover: EventEmitter<LieuMediationNumerique> = new EventEmitter<LieuMediationNumerique>();
 }
