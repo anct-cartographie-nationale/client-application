9f64eee0ab0c004bee01b3793532f64086931d63
Marc Gavanier <marc.gavanier@gmail.com>
2022-07-20 20:38:33 +0200

    feat: update besoin page

--- src/features/orientation/infrastructure/presentation/pages/besoin/besoin.page.ts
+++ src/features/orientation/infrastructure/presentation/pages/besoin/besoin.page.ts
@@ -2,19 +2,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { OrientationLayout } from '../../layouts';
 import { OrientationItemPresentation } from '../../../../domain/presenters';
 import demarches from './demarches.json';
-import miseANiveau from './miseANiveau.json';
-import manqueDeMateriel from './manqueDeMateriel.json';
+import miseANiveau from './mise-a-niveau.json';
+import cultureNumerique from './culture-numerique.json';
+import manqueDeMateriel from './manque-de-materiel.json';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './besoin.page.html'
 })
 export class BesoinPage {
-  public demarcheOrientationItems: OrientationItemPresentation<string>[] = demarches;
+  public demarchesOrientationItems: OrientationItemPresentation<string>[] = demarches;
+
+  public cultureNumeriqueOrientationItems: OrientationItemPresentation<string>[] = cultureNumerique;
 
   public miseANiveauOrientationItems: OrientationItemPresentation<string>[] = miseANiveau;
 
-  public manqueDeMaterielItems: OrientationItemPresentation<string>[] = manqueDeMateriel;
+  public manqueDeMaterielOrientationItems: OrientationItemPresentation<string>[] = manqueDeMateriel;
 
   public constructor(public readonly orientationLayout: OrientationLayout) {}
 }
