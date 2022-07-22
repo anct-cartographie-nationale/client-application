ce200679910237523737668a8449569a2092549c
Marc Gavanier <marc.gavanier@gmail.com>
2022-07-22 18:46:45 +0200

    style: fix information generale style

--- src/features/cartographie/infrastructure/presentation/components/informations-generales/informations-generales.component.ts
+++ src/features/cartographie/infrastructure/presentation/components/informations-generales/informations-generales.component.ts
@@ -9,6 +9,7 @@ export class InformationsGeneralesComponent {
   @Input() public nom: string = '';
   @Input() public typologie?: string;
   @Input() public date?: Date;
+  @Input() public status?: 'Ouvert' | 'Fermé';
 
   public constructor(private readonly router: Router) {}
 
