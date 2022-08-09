d83636ec667c0d686ad9e3eb963c342dca33e369
Marc Gavanier <marc.gavanier@gmail.com>
2022-08-09 18:54:34 +0200

    fix: remove unused highlight in marker presenter

--- src/features/cartographie/domain/presenters/markers/markers.presenter.ts
+++ src/features/cartographie/domain/presenters/markers/markers.presenter.ts
@@ -47,17 +47,10 @@ export const getBoundFromLocalisations = (localisations: Localisation[]): [Local
 
 export class MarkersPresenter {
   private readonly _centerView$: Subject<CenterView> = new Subject<CenterView>();
-  private readonly _highlighted$: BehaviorSubject<string> = new BehaviorSubject<string>('');
   private readonly _selected$: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
   public readonly centerView$: Observable<CenterView> = this._centerView$.asObservable();
-  public readonly highlighted$: Observable<string> = this._highlighted$.asObservable();
   public readonly selected$: Observable<string> = this._selected$.asObservable();
-
-  public highlight(markerId: string) {
-    this._highlighted$.next(markerId);
-    this._selected$.next('');
-  }
 
   public select(markerId: string) {
     this._selected$.next(markerId);
