9e2951a9f24549f09aac97e69b7526abfff34b15
Marc Gavanier <marc.gavanier@gmail.com>
2022-08-10 17:49:15 +0200

    feat: add scroll and focus on scrolled item in list page

--- src/features/cartographie/infrastructure/presentation/pages/lieux-mediation-numerique-list/lieux-mediation-numerique-list.page.ts
+++ src/features/cartographie/infrastructure/presentation/pages/lieux-mediation-numerique-list/lieux-mediation-numerique-list.page.ts
@@ -4,18 +4,61 @@ import { MarkersPresenter } from '../../../../domain';
 import { LieuMediationNumerique } from '../../../../../../models';
 import { CartographieLayout } from '../../layouts';
 import { LieuMediationNumeriqueListItemPresentation } from '../../../../domain/presenters/lieux-mediation-numerique-list/lieu-mediation-numerique-list-item.presentation';
+import { combineLatest, Observable, tap } from 'rxjs';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { map } from 'rxjs/operators';
+
+const toLieuxWithLieuToFocus = ([lieux, paramMap]: [LieuMediationNumeriqueListItemPresentation[], ParamMap]): [
+  LieuMediationNumeriqueListItemPresentation[],
+  LieuMediationNumeriqueListItemPresentation?
+] => [lieux, lieux.find((lieu: LieuMediationNumeriqueListItemPresentation) => lieu.id === paramMap.get('id'))];
+
+const toLieux = ([lieux]: [
+  LieuMediationNumeriqueListItemPresentation[],
+  LieuMediationNumeriqueListItemPresentation?
+]): LieuMediationNumeriqueListItemPresentation[] => {
+  return lieux;
+};
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: 'lieux-mediation-numerique-list.page.html'
 })
 export class LieuxMediationNumeriqueListPage {
+  private _initialZoom: boolean = false;
+
+  private setInitialState = ([_, lieu]: [
+    LieuMediationNumeriqueListItemPresentation[],
+    LieuMediationNumeriqueListItemPresentation?
+  ]): void => {
+    this.markersPresenter.select('');
+    lieu && !this._initialZoom && this.focusOnLieu(lieu);
+    this._initialZoom = true;
+  };
+
+  public lieuxMediationNumerique$: Observable<LieuMediationNumeriqueListItemPresentation[]> = combineLatest([
+    this._cartographieLayout.lieuxMediationNumerique$,
+    this._route.paramMap
+  ]).pipe(map(toLieuxWithLieuToFocus), tap(this.setInitialState), map(toLieux));
+
   public constructor(
     @Inject(ZOOM_LEVEL_TOKEN)
     private readonly _zoomLevel: ZoomLevelConfiguration,
-    public readonly markersPresenter: MarkersPresenter,
-    public readonly cartographieLayout: CartographieLayout
+    private readonly _cartographieLayout: CartographieLayout,
+    private readonly _route: ActivatedRoute,
+    public readonly markersPresenter: MarkersPresenter
   ) {}
+
+  private focusOnLieu(lieu: LieuMediationNumeriqueListItemPresentation) {
+    this.markersPresenter.focus(lieu.localisation, this._zoomLevel.userPosition);
+
+    setTimeout((): void => {
+      document.getElementById(lieu.id)?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest'
+      });
+    });
+  }
 
   public trackByLieuId(_: number, lieu: LieuMediationNumeriqueListItemPresentation) {
     return lieu.id;
