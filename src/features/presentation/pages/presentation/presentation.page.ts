389750e2fdab3494ece814bf1de74b864dabf8fb
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2022-08-23 17:46:32 +0200

    feat: carousel update

--- src/features/presentation/pages/presentation/presentation.page.ts
+++ src/features/presentation/pages/presentation/presentation.page.ts
@@ -20,9 +20,39 @@ import {
   styleUrls: ['./presentation.page.scss']
 })
 export class PresentationLayout {
+  public currentSlide: number = 0;
   public environment: Boolean = environment.production;
   public lieuxMediationNumeriqueTotal$: Observable<LieuMediationNumeriquePresentation[]> =
     this._lieuxMediationNumeriqueListPresenter.lieuxMediationNumeriqueTotal$();
 
   public constructor(private readonly _lieuxMediationNumeriqueListPresenter: LieuxMediationNumeriquePresenter) {}
+
+  // carousel navigator
+  switchSlide = (value: number): void => {
+    this.currentSlide = value;
+  };
+
+  onPreviousClick = (): void => {
+    this.currentSlide = this.currentSlide - 1 < 0 ? 3 : this.currentSlide - 1;
+  };
+
+  onNextClick = (): void => {
+    this.currentSlide = this.currentSlide + 1 > 3 ? 0 : this.currentSlide + 1;
+  };
+
+  setBackgroundForEachSlides = (): string => {
+    let activeClass = '';
+    switch (this.currentSlide) {
+      case 0:
+        return (activeClass = 'bg-orientation');
+      case 1:
+        return (activeClass = 'bg-visibilite');
+      case 2:
+        return (activeClass = 'bg-centralisation');
+      case 3:
+        return (activeClass = 'bg-actualisation');
+      default:
+        return (activeClass = 'bg-orientation');
+    }
+  };
 }
