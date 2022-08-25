236da4bf9afb6c78b77b4d705f1043b704b9b197
Arthur Belkhayat <arthur.belkhayat@gmail.com>
2022-08-25 14:49:21 +0200

    fix: filterForm reset on disponibilite page (#88)
    * fix: filterForm reset on disponibilite page
    
    * fix: pr comments

--- src/features/orientation/layouts/orientation/orientation.layout.ts
+++ src/features/orientation/layouts/orientation/orientation.layout.ts
@@ -2,7 +2,7 @@ import { HttpParams } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, ChildrenOutletContexts, Router } from '@angular/router';
-import { delay, Observable, startWith, tap } from 'rxjs';
+import { delay, Observable, startWith, Subject, tap } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { FEATURES_TOKEN, FeaturesConfiguration } from '../../../../root';
 import {
@@ -54,6 +54,9 @@ const createFormGroupFromFilterPresentation = (filterPresentation: FilterPresent
   ]
 })
 export class OrientationLayout {
+  private _lieuxMediationNumeriqueCount$: Subject<number> = new Subject<number>();
+  public lieuxMediationNumeriqueCount$: Observable<number> = this._lieuxMediationNumeriqueCount$.asObservable();
+
   public filterForm: FormGroup = createFormGroupFromFilterPresentation(
     toFilterFormPresentationFromQuery(this.route.snapshot.queryParams)
   );
@@ -71,8 +74,13 @@ export class OrientationLayout {
     tap(this.setFilterToQueryString())
   );
 
-  public lieuxMediationNumerique$: Observable<LieuMediationNumeriquePresentation[]> =
-    this._lieuxMediationNumeriqueListPresenter.lieuxMediationNumeriqueByDistance$(this._localisation$, this._filter$);
+  public lieuxMediationNumerique$: Observable<LieuMediationNumeriquePresentation[]> = this._lieuxMediationNumeriqueListPresenter
+    .lieuxMediationNumeriqueByDistance$(this._localisation$, this._filter$)
+    .pipe(
+      tap((lieuxMediationNumerique: LieuMediationNumeriquePresentation[]) =>
+        this._lieuxMediationNumeriqueCount$.next(lieuxMediationNumerique.length)
+      )
+    );
 
   public lieuxMediationNumeriqueTotal$: Observable<LieuMediationNumerique[]> =
     this._lieuxMediationNumeriqueListPresenter.lieuxMediationNumeriqueTotal$;
