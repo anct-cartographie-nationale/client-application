94d84064f8a9cde52418f4883a4eb9304ab2b30e
Marc Gavanier <marc.gavanier@gmail.com>
2022-08-29 23:53:23 +0200

    fix: use route config instead of component name which can be minified

--- src/features/cartographie/presenters/zoom-level-navigation/zoom-level-navigation.presenter.ts
+++ src/features/cartographie/presenters/zoom-level-navigation/zoom-level-navigation.presenter.ts
@@ -15,13 +15,14 @@ export const getNextRouteFromZoomLevel = (currentZoomLevel: number) => {
   return DEPARTEMENTS_ROUTE;
 };
 
-const isRegion = (route: string, pageName: string): boolean => route === REGIONS_ROUTE && pageName !== 'RegionsPage';
+const isRegion = (route: string, routeConfigPath: string): boolean => route === REGIONS_ROUTE && routeConfigPath !== 'regions';
 
-const isDepartement = (route: string, pageName: string): boolean =>
-  route === DEPARTEMENTS_ROUTE && pageName !== 'DepartementsPage';
+const isDepartement = (route: string, routeConfigPath: string): boolean =>
+  route === DEPARTEMENTS_ROUTE && !['regions/:nomRegion', 'departements'].includes(routeConfigPath);
 
-const isLieux = (route: string, pageName: string): boolean =>
-  route === LIEUX_ROUTE && pageName !== 'LieuxMediationNumeriqueListPage';
+const isLieux = (route: string, routeConfigPath: string): boolean =>
+  route === LIEUX_ROUTE && routeConfigPath !== 'regions/:nomRegion/:nomDepartement';
 
-export const shouldNavigateToListPage = (route: string, pageName?: string): boolean =>
-  pageName != null && (isDepartement(route, pageName) || isRegion(route, pageName) || isLieux(route, pageName));
+export const shouldNavigateToListPage = (route: string, routeConfigPath?: string): boolean =>
+  routeConfigPath != null &&
+  (isDepartement(route, routeConfigPath) || isRegion(route, routeConfigPath) || isLieux(route, routeConfigPath));
