f360d0f9bddb18b245ab32bba99953031d59e45c
Arthur Belkhayat <arthur.belkhayat@gmail.com>
2022-09-26 13:30:29 +0200

    feat: data assembleur with default theme (#127)

--- src/root/configuration/data.configuration.ts
+++ src/root/configuration/data.configuration.ts
@@ -8,7 +8,5 @@ export type DataConfiguration = {
 export const DATA_TOKEN: InjectionToken<DataConfiguration> = new InjectionToken<DataConfiguration>('data.configuration');
 
 export const DATA_CONFIGURATION: DataConfiguration = {
-  lieuxDeMediationNumerique: environment.production
-    ? 'https://api.conseiller-numerique.gouv.fr/permanences'
-    : '/assets/data/lieux-de-mediation-numerique.local-fake.json'
+  lieuxDeMediationNumerique: '/assets/data/data-normalize-hauts-de-france.json'
 };
