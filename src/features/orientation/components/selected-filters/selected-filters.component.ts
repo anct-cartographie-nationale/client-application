31f7b64ddb99fff968d8480563cf6332e2d25d14
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2022-10-03 16:30:41 +0200

    fix: display label instead of value for chip widget

--- src/features/orientation/components/selected-filters/selected-filters.component.ts
+++ src/features/orientation/components/selected-filters/selected-filters.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { OrientationItemPresentation } from '@features/orientation/presenters';
 import { FilterFormPresentation } from '../../../core';
+import typeAccompagnements from '../../pages/accessibilite/type-accompagnements.json';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -10,6 +12,7 @@ import { FilterFormPresentation } from '../../../core';
 export class SelectedFiltersComponent {
   @Input() public filterPresentation: FilterFormPresentation | null = null;
   @Input() public filterForm!: FormGroup;
+  public typeAccompagnementsOptions: OrientationItemPresentation<string>[] = typeAccompagnements;
 
   public resetForm(value: string | number, key: string) {
     if (key === 'services') this.filterForm.get('services')?.setValue('');
@@ -33,5 +36,13 @@ export class SelectedFiltersComponent {
     if (distance === 100000 || distance === '100000') return 'Moins de 100 km';
     else if (distance === 20000 || distance === '20000') return 'Moins de 20 km';
     else return 'Moins de 5 km';
+  }
+
+  public getLabelOfAccess(value: string): string {
+    let labelOfAccess: string = '';
+    typeAccompagnements.forEach((field: { value: string; label: string }) => {
+      if (field.value === value) labelOfAccess = field.label;
+    });
+    return labelOfAccess;
   }
 }
