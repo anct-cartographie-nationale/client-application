07c9f8bfeae34ddb385aa7d240408e47ec54ba33
Arthur Belkhayat <arthur.belkhayat@gmail.com>
2022-10-17 17:46:13 +0200

    fix: change wording carousel presentation page (#151)
    * fix: change wording carousel presentation page
    
    * fix: somes changes
    
    * fix: conflits

--- src/features/presentation/components/carousel/carousel.component.ts
+++ src/features/presentation/components/carousel/carousel.component.ts
@@ -31,34 +31,30 @@ export class CarouselComponent {
 
   public slidePresentations: SlidePresentations[] = [
     {
-      titre: "L'orientation",
-      text: `En 4 étapes, vous pouvez afficher les structures correspondant exactement aux besoins d'un bénéficiaire, dans une zone
-    géographique limitée ouvertes lorsqu'il est disponible.`,
+      titre: 'Orientation',
+      text: `En quelques secondes, renseignez les besoins d'un bénéficiaire, son adresse et ses disponibilités afin d'afficher uniquement les structures qui pourront l'aider.`,
       imageUrl: '/assets/img/presentation/svg-orientation-vertical.svg',
       imageDescription: ''
     },
     {
-      titre: 'La visibilité',
-      text: `L'idéologie ici est de proposer un outil à la fois national et local. La base de données est accessible à tous et
-    contributive : de la plus petite collectivité à un hub, chacun peut implémenter sur son site cet outil et afficher
-    l'offre de médiation de son territoire. En renseignant vos données sur la cartographie, ce n'est pas uniquement les
-    usagers du site national qui auront accès aux informations de votre structure, mais également tout les usagers des
-    sites web des collectivités qui auront intégré la plateforme !`,
-      imageUrl: '/assets/img/presentation/svg-visibilite-vertical.svg',
-      imageDescription: ''
-    },
-    {
-      titre: 'La centralisation',
-      text: `Travailler sur cette base de donnes commune permet d'avoir une gestion de la donnée centralisée, davantage actualisée
-    et permettra d'améliorer la visibilité et faire gagner du temps aux professionnels de la médiation numérique.`,
+      titre: 'Standardisation',
+      text: '',
       imageUrl: '/assets/img/presentation/svg-centralisation-vertical.svg',
       imageDescription: ''
     },
     {
-      titre: "L'actualisation",
-      text: `Notre proximité avec les structures de médiation numérique nous permet d'assurer des données actualisées il y a moins
-    de 3 mois et ainsi éviter aux bénéficiaires de se rendre dans une structure fermée.`,
+      titre: 'La mise à jour',
+      text: `En collaboration avec Dora, le service public numérique de recensement et mise à jour de l’offre d’insertion, 
+      nous proposons à tous les acteurs de la médiation numérique de référencer et mettre à jour leur offre à un seul endroit tout en la rendant disponible partout,
+      grâce à l'open data.`,
       imageUrl: '/assets/img/presentation/svg-actu-vertical.svg',
+      imageDescription: ''
+    },
+    {
+      titre: 'Visibilité',
+      text: `Cette cartographie nationale est locale, la base de données est accessible à tous et contributive : de la plus petite collectivité à un hub,
+       chacun peut l'implémenter sur son site et afficher l'offre de médiation numérique de son territoire.`,
+      imageUrl: '/assets/img/presentation/svg-visibilite-vertical.svg',
       imageDescription: ''
     }
   ];
