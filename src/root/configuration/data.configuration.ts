b2b2042149c457732fe017198f47caabb213a3d0
Marc Gavanier <marc.gavanier@gmail.com>
2022-11-08 12:11:44 +0100

    fix: update package version (#152)

--- src/root/configuration/data.configuration.ts
+++ src/root/configuration/data.configuration.ts
@@ -8,5 +8,6 @@ export type DataConfiguration = {
 export const DATA_TOKEN: InjectionToken<DataConfiguration> = new InjectionToken<DataConfiguration>('data.configuration');
 
 export const DATA_CONFIGURATION: DataConfiguration = {
-  lieuxDeMediationNumerique: '/assets/data/data-normalize-hauts-de-france.json'
+  lieuxDeMediationNumerique:
+    'https://cdn.jsdelivr.net/npm/@gouvfr-anct/cartographie-nationale@4.4.0/assets/data/data-normalize-hauts-de-france.json'
 };
