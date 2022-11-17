1c55e2b23cf0c5acb05723501beca54b350c14fb
Marc Gavanier <marc.gavanier@gmail.com>
2022-11-17 12:14:55 +0100

    fix: use primary color for user marker

--- src/features/cartographie/configuration/marker/marker-factories/user.marker-factory.ts
+++ src/features/cartographie/configuration/marker/marker-factories/user.marker-factory.ts
@@ -11,8 +11,8 @@ const USER_ICON_ANCHOR: LeafletPoint = new LeafletPoint(USER_MARKER_DIMENSIONS.x
 
 const userIconMarkerSvg = (): string => `
 <svg width="28" height="28" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg">
-  <circle style="fill:#000;fill-opacity:.54902;stroke-width:19.9299;stroke-linecap:square;paint-order:markers fill stroke" cx="14" cy="14" r="14"/>
-  <circle style="fill:#0f0;fill-opacity:1;stroke-width:18.9946;stroke-linecap:square;paint-order:markers fill stroke" cx="14" cy="14" r="9"/>
+  <circle cx="14" cy="14" r="14" fill-opacity=".3"/>
+  <circle class="user-marker" cx="14" cy="14" r="9" fill-opacity=".65"/>
 </svg>`;
 
 const USER_DIV_ICON = (): DivIcon =>
