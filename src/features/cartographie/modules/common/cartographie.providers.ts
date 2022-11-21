17160d0d64011ad42336fe4eb43b3bf7d670a2a1
Marc Gavanier <marc.gavanier@gmail.com>
2022-11-21 16:27:44 +0100

    fix: missing address repository in cartographie providers

--- src/features/cartographie/modules/common/cartographie.providers.ts
+++ src/features/cartographie/modules/common/cartographie.providers.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { INITIAL_POSITION_TOKEN, ZOOM_LEVEL_TOKEN } from '@gouvfr-anct/mediation-numerique';
 import { POSITION_CONFIGURATION, ZOOM_LEVEL_CONFIGURATION } from '../../../../root';
+import { AddressHttp, AddressRepository } from '../../../adresse';
 
 export const cartographieProviders = [
   {
@@ -9,5 +11,10 @@ export const cartographieProviders = [
   {
     provide: ZOOM_LEVEL_TOKEN,
     useValue: ZOOM_LEVEL_CONFIGURATION
+  },
+  {
+    deps: [HttpClient],
+    provide: AddressRepository,
+    useClass: AddressHttp
   }
 ];
