2ec941f9e136668c9c29744beafc131a73d10361
Marc Gavanier <marc.gavanier@gmail.com>
2022-12-12 16:42:42 +0100

    feat: link data gouv data inclusion dataset as data source

--- src/root/configuration/data.configuration.ts
+++ src/root/configuration/data.configuration.ts
@@ -8,5 +8,5 @@ export type DataConfiguration = {
 export const DATA_TOKEN: InjectionToken<DataConfiguration> = new InjectionToken<DataConfiguration>('data.configuration');
 
 export const DATA_CONFIGURATION: DataConfiguration = {
-  lieuxDeMediationNumerique: 'https://www.data.gouv.fr/fr/datasets/r/da914fdc-f073-4074-9d7c-87efc856824e'
+  lieuxDeMediationNumerique: 'https://www.data.gouv.fr/fr/datasets/r/b5e5a1e1-122e-4f87-b6cf-d1ce342671be'
 };
