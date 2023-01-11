6268bbad3dcd26c9a2f1f80a0ac51736eb3af616
Marc Gavanier <marc.gavanier@gmail.com>
2023-01-11 10:56:31 +0100

    feat: use les assembleurs data

--- src/root/configuration/data.configuration.ts
+++ src/root/configuration/data.configuration.ts
@@ -8,5 +8,5 @@ export type DataConfiguration = {
 export const DATA_TOKEN: InjectionToken<DataConfiguration> = new InjectionToken<DataConfiguration>('data.configuration');
 
 export const DATA_CONFIGURATION: DataConfiguration = {
-  lieuxDeMediationNumerique: 'https://www.data.gouv.fr/fr/datasets/r/b5e5a1e1-122e-4f87-b6cf-d1ce342671be'
+  lieuxDeMediationNumerique: 'https://www.data.gouv.fr/fr/datasets/r/9cb4d69b-f666-4d4c-a3b3-d119809f28a8'
 };
