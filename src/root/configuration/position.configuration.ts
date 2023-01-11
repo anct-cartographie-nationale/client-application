8d4bae559aa9d6c2f605bdbcb54a68407d3f8c54
Marc Gavanier <marc.gavanier@gmail.com>
2023-01-11 10:57:12 +0100

    feat: set les hauts de france default location and zoom

--- src/root/configuration/position.configuration.ts
+++ src/root/configuration/position.configuration.ts
@@ -1,6 +1,6 @@
 import { InitialPositionConfiguration } from '@gouvfr-anct/mediation-numerique';
 
 export const POSITION_CONFIGURATION: InitialPositionConfiguration = {
-  latitude: 46.28146057911664,
-  longitude: 4.468874066180609
+  latitude: 50,
+  longitude: 3.4
 };
