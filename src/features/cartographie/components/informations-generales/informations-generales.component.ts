4c798a7bbedc12e5ee6d1d425d587c7a686b6901
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2023-01-18 12:39:45 +0100

    refactor: redirection link source

--- src/features/cartographie/components/informations-generales/informations-generales.component.ts
+++ src/features/cartographie/components/informations-generales/informations-generales.component.ts
@@ -1,6 +1,11 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { OpeningStatus } from '../../../core';
 
+type sourcePresentation = {
+  nom: string;
+  link: string;
+};
+
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-informations-generales',
@@ -13,4 +18,31 @@ export class InformationsGeneralesComponent {
   @Input() public date?: Date;
   @Input() public status?: OpeningStatus;
   @Input() public source?: string = '';
+
+  public sourceRedirectionLink: sourcePresentation[] = [
+    {
+      nom: 'conseiller-numerique',
+      link: 'https://www.conseiller-numerique.gouv.fr'
+    },
+    {
+      nom: 'dora',
+      link: 'https://dora.fabrique.social.gouv.fr'
+    },
+    {
+      nom: 'hinaura',
+      link: 'https://www.hinaura.fr'
+    },
+    {
+      nom: 'maine-et-loire',
+      link: 'https://data.maine-et-loire.fr/explore/dataset/224900019_lieux-de-mediation-numerique-en-maine-et-loire/table/https://data.maine-et-loire.fr/explore/dataset/224900019_lieux-de-mediation-numerique-en-maine-et-loire/table/'
+    },
+    {
+      nom: 'Les Assembleurs',
+      link: 'https://www.data.gouv.fr/fr/datasets/lieux-de-mediation-numerique-sur-le-territoire-hauts-de-france-fournis-par-le-hub-les-assembleurs/'
+    },
+    {
+      nom: 'france-services',
+      link: 'https://www.data.gouv.fr/fr/datasets/liste-des-structures-labellisees-france-services/'
+    }
+  ];
 }
