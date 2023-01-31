dfb69252acad84b3cac3fb78ad1cc34e9a3d4223
Marc Gavanier <marc.gavanier@gmail.com>
2023-01-31 09:02:26 +0100

    feat: add cartographie nationale logo

--- src/root/configuration/brand.configuration.ts
+++ src/root/configuration/brand.configuration.ts
@@ -13,3 +13,9 @@ export const BRAND_CONFIGURATION: BrandConfiguration = {
   logo: '',
   illustration: 'svg-france-et-outremer.svg'
 };
+
+export const BRAND_APPLICATION_CONFIGURATION: BrandConfiguration = {
+  name: 'Nationale',
+  logo: './assets/img/logo/cartographie-nationale.png',
+  illustration: 'svg-france-et-outremer.svg'
+};
