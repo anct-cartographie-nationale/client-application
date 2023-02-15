4ff7253f1908c2f2cb0d8211f82e747386eb5b5b
Marc Gavanier <marc.gavanier@gmail.com>
2023-02-15 15:08:14 +0100

    fix: update orientation email message

--- src/features/cartographie/pages/lieux-mediation-numerique-details/lieux-mediation-numerique-details.presentation.spec.ts
+++ src/features/cartographie/pages/lieux-mediation-numerique-details/lieux-mediation-numerique-details.presentation.spec.ts
@@ -22,7 +22,7 @@ describe('lieux mediation numerique details presentation', (): void => {
     );
 
     expect(message).toBe(
-      `Bonjour 👋,%0D%0A%0D%0AVoici le lieu de médiation numérique qui vous a été partagé à l'issue du parcours d'orientation :%0D%0A%0D%0AClub Lauriston%0D%0A%0D%0A78 rue Lauriston 75016 Paris%0D%0A%0D%0AServices disponibles :%0D%0A- Prendre en main un smartphone ou une tablette%0D%0A- Prendre en main un ordinateur%0D%0A- Utiliser le numérique au quotidien%0D%0A- Approfondir ma culture numérique%0D%0A%0D%0ARetrouvez toutes ces informations sur la cartographie : https://cartographie.societenumerique.gouv.fr/cartographie/6303493719ed1006e01aa243/details%0D%0A%0D%0AÀ bientôt sur la cartographie nationale des lieux de médiation numérique !`
+      'Bonjour 👋,%0D%0A%0D%0AVoici les informations du lieu qui saura vous accueillir et vous proposer un accompagnement au numérique :%0D%0A%0D%0AClub Lauriston%0D%0A%0D%0A78 rue Lauriston 75016 Paris%0D%0A%0D%0AServices disponibles :%0D%0A- Prendre en main un smartphone ou une tablette%0D%0A- Prendre en main un ordinateur%0D%0A- Utiliser le numérique au quotidien%0D%0A- Approfondir ma culture numérique%0D%0A%0D%0ARetrouvez toutes les informations de ce lieu sur le site web de la cartographie : https://cartographie.societenumerique.gouv.fr/cartographie/6303493719ed1006e01aa243/details%0D%0A%0D%0ANumériquement, à bientôt !'
     );
   });
 
@@ -39,8 +39,7 @@ describe('lieux mediation numerique details presentation', (): void => {
       ],
       contact: Contact({
         telephone: '+33 1 85 53 99 74',
-        courriel: 'infocontact@universcience.fr',
-        site_web: [Url('https://www.cite-sciences.fr/fr/accueil')]
+        courriel: 'infocontact@universcience.fr'
       })
     };
 
@@ -50,7 +49,7 @@ describe('lieux mediation numerique details presentation', (): void => {
     );
 
     expect(message).toBe(
-      "Bonjour 👋,%0D%0A%0D%0AVoici le lieu de médiation numérique qui vous a été partagé à l'issue du parcours d'orientation :%0D%0A%0D%0ACité des métiers / Universcience%0D%0A%0D%0AAV FRANKLIN D ROOSEVELT 75008 Paris 8%0D%0A%0D%0AServices disponibles :%0D%0A- Prendre en main un smartphone ou une tablette%0D%0A- Prendre en main un ordinateur%0D%0A- Utiliser le numérique au quotidien%0D%0A- Approfondir ma culture numérique%0D%0A%0D%0ANuméro de téléphone : +33 1 85 53 99 74%0D%0ACourriel : infocontact@universcience.fr%0D%0ASite internet : https://www.cite-sciences.fr/fr/accueil%0D%0A%0D%0ARetrouvez toutes ces informations sur la cartographie : https://cartographie.societenumerique.gouv.fr/cartographie/638622e80830e306f21ecc64/details%0D%0A%0D%0AÀ bientôt sur la cartographie nationale des lieux de médiation numérique !"
+      'Bonjour 👋,%0D%0A%0D%0AVoici les informations du lieu qui saura vous accueillir et vous proposer un accompagnement au numérique :%0D%0A%0D%0ACité des métiers / Universcience%0D%0A%0D%0AAV FRANKLIN D ROOSEVELT 75008 Paris 8%0D%0A%0D%0A+33 1 85 53 99 74%0D%0ACinfocontact@universcience.fr%0D%0A%0D%0AServices disponibles :%0D%0A- Prendre en main un smartphone ou une tablette%0D%0A- Prendre en main un ordinateur%0D%0A- Utiliser le numérique au quotidien%0D%0A- Approfondir ma culture numérique%0D%0A%0D%0ARetrouvez toutes les informations de ce lieu sur le site web de la cartographie : https://cartographie.societenumerique.gouv.fr/cartographie/638622e80830e306f21ecc64/details%0D%0A%0D%0ANumériquement, à bientôt !'
     );
   });
 });
--- src/features/cartographie/pages/lieux-mediation-numerique-details/lieux-mediation-numerique-details.presentation.ts
+++ src/features/cartographie/pages/lieux-mediation-numerique-details/lieux-mediation-numerique-details.presentation.ts
@@ -3,31 +3,25 @@ import { LieuMediationNumeriqueDetailsPresentation } from '../../presenters';
 
 const formatServices = (services: Service[]): string => `${services.join('%0D%0A- ')}`;
 
-const formatTelephoneIfAny = (telephone?: string): string => (telephone ? `%0D%0ANuméro de téléphone : ${telephone}` : '');
+const formatTelephoneIfAny = (telephone?: string): string => (telephone ? `%0D%0A${telephone}` : '');
 
-const formatCourrielIfAny = (telephone?: string): string => (telephone ? `%0D%0ACourriel : ${telephone}` : '');
-
-const formatSiteWebIfAny = (site_web?: string): string => (site_web ? `%0D%0ASite internet : ${site_web}` : '');
+const formatCourrielIfAny = (telephone?: string): string => (telephone ? `%0D%0AC${telephone}` : '');
 
 const formatContactIfAny = (contact?: Contact): string =>
-  contact
-    ? `${formatTelephoneIfAny(contact.telephone)}${formatCourrielIfAny(contact.courriel)}${formatSiteWebIfAny(
-        contact.site_web?.[0]
-      )}%0D%0A`
-    : '';
+  contact ? `${formatTelephoneIfAny(contact.telephone)}${formatCourrielIfAny(contact.courriel)}%0D%0A` : '';
 
 export const emailMessage = (lieu: LieuMediationNumeriqueDetailsPresentation, detailsLink: string): string =>
   `Bonjour 👋,
 
-Voici le lieu de médiation numérique qui vous a été partagé à l'issue du parcours d'orientation :
+Voici les informations du lieu qui saura vous accueillir et vous proposer un accompagnement au numérique :
 
 ${lieu.nom}
 
 ${lieu.adresse}
-
+${formatContactIfAny(lieu.contact)}
 Services disponibles :
 - ${formatServices(lieu.services)}
-${formatContactIfAny(lieu.contact)}
-Retrouvez toutes ces informations sur la cartographie : ${detailsLink}
 
-À bientôt sur la cartographie nationale des lieux de médiation numérique !`.replace(/\n/gu, '%0D%0A');
+Retrouvez toutes les informations de ce lieu sur le site web de la cartographie : ${detailsLink}
+
+Numériquement, à bientôt !`.replace(/\n/gu, '%0D%0A');
