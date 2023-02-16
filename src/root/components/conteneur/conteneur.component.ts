58b50a2d3c61cc15425e3c6cdce65ce34d5aa399
Marc Gavanier <marc.gavanier@gmail.com>
2023-02-16 10:22:37 +0100

    feat: add navbar collapse

--- src/root/components/conteneur/conteneur.component.ts
+++ src/root/components/conteneur/conteneur.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, Inject, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { BRAND_CONFIGURATION, BRAND_TOKEN, BrandConfiguration } from '../../configuration';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,8 +17,29 @@ export class ConteneurComponent {
     BRAND_CONFIGURATION.name = name;
   }
 
+  private _height$: Subject<number> = new Subject<number>();
+  public height$: Observable<number> = this._height$.asObservable();
+
+  private _collapsing$: Subject<boolean> = new Subject<boolean>();
+  public collapsing$: Observable<boolean> = this._collapsing$.asObservable();
+
+  private _isExpanded: boolean = false;
+
+  private _expanded$: Subject<boolean> = new Subject<boolean>();
+  public expanded$: Observable<boolean> = this._expanded$.asObservable();
+
   public constructor(
     @Inject(BRAND_TOKEN) public readonly brandConfiguration: BrandConfiguration,
     public readonly router: Router
   ) {}
+
+  public toggle() {
+    this._isExpanded = !this._isExpanded;
+    this._expanded$.next(this._isExpanded);
+    this._height$.next(this._isExpanded ? 196 : 0);
+    this._collapsing$.next(true);
+    setTimeout(() => {
+      this._collapsing$.next(false);
+    }, 0);
+  }
 }
