b5ff38104ca76bb4860bba2f76fc38df7d684e6f
Marc Gavanier <marc.gavanier@gmail.com>
2023-02-21 09:47:27 +0100

    feat: merge region and departement markers factories to territoire

--- src/features/cartographie/configuration/marker/markers.token.ts
+++ src/features/cartographie/configuration/marker/markers.token.ts
@@ -2,8 +2,7 @@ import { InjectionToken } from '@angular/core';
 import { DivIcon, Icon } from 'leaflet';
 import { MarkersConfiguration } from './markers.configuration';
 import { lieuMediationNumeriqueMarkerFactory } from './marker-factories/lieu-mediation-numerique.marker-factory';
-import { departementMarkerFactory } from './marker-factories/departement.marker-factory';
-import { regionMarkerFactory } from './marker-factories/region.marker-factory';
+import { territoireMarkerFactory } from './marker-factories/territoire.marker-factory';
 import { userMarkerFactory } from './marker-factories/user.marker-factory';
 
 export const MARKERS_TOKEN: InjectionToken<MarkersConfiguration<never, DivIcon | Icon>> = new InjectionToken<
@@ -12,14 +11,12 @@ export const MARKERS_TOKEN: InjectionToken<MarkersConfiguration<never, DivIcon |
 
 export enum MarkerKey {
   LieuMediationNumerique = 'lieuMediationNumerique',
-  Departement = 'departement',
-  Region = 'region',
+  Territoire = 'territoire',
   User = 'user'
 }
 
 export const MARKERS: MarkersConfiguration<never, DivIcon | Icon> = {
   [MarkerKey.LieuMediationNumerique]: lieuMediationNumeriqueMarkerFactory,
-  [MarkerKey.Departement]: departementMarkerFactory,
-  [MarkerKey.Region]: regionMarkerFactory,
+  [MarkerKey.Territoire]: territoireMarkerFactory,
   [MarkerKey.User]: userMarkerFactory
 };
