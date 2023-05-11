b76e224c376382ebc8d280400c78439fa57090ca
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2023-05-11 13:17:11 +0200

    fix: wording

--- src/features/orientation/pages/accessibilite/accessibilite-information-modal-texts.ts
+++ src/features/orientation/pages/accessibilite/accessibilite-information-modal-texts.ts
@@ -13,11 +13,11 @@ export const ACCESSIBILITE_INFORMATION_MODAL_TEXTS: Record<
     description:
       "La participation est le tarif pouvant conditionner l'accès à une structure ou un service de médiation numérique. Si vous sélectionnez certaines de ces options, vous restreindrez votre recherche aux structures demandant ces participations",
     elements: [
-      'Gratuit : Accès gratuit au lieu et à ses services',
-      'Gratuit sous condition : situation familiale, convention avec un organisme social, etc.',
-      'Payant : L’accès au lieu et/ou à ses services est payant',
-      'Adhésion : L’accès au lieu et/ou à ses services nécessite d’y adhérer',
-      'Pass numérique : Il est possible d’utiliser un Pass numérique pour accéder au lieu'
+      'Gratuit : accès gratuit au lieu et à ses services',
+      'Gratuit sous condition : accès gratuit selon une situation familiale, convention avec un organisme social, etc.',
+      'Payant : l’accès au lieu et/ou à ses services est payant',
+      'Adhésion : l’accès au lieu et/ou à ses services nécessite d’y adhérer',
+      'Pass numérique : il est possible d’utiliser un Pass numérique pour accéder au lieu'
     ],
     label: 'Détails des différentes options'
   },
@@ -25,8 +25,8 @@ export const ACCESSIBILITE_INFORMATION_MODAL_TEXTS: Record<
     titre: 'Accueil adapté',
     description: `Si vous sélectionnez certaines de ces options, vous restreindrez votre recherche aux structures en capacité d'accueillir certains types de publics. Pour toute information concernant les déplacements des personnes en situation de handicap, rendez-vous sur <a href="https://acceslibre.beta.gouv.fr/" target="_blank" rel="noopener noreferrer">Accès libre</a>`,
     elements: [
-      'Handicaps psychique : troubles psychiatriques donnant lieu à des atteintes comportementales',
-      'Handicaps mental : déficiences limitant les activités d’une personne'
+      'Handicap psychique : trouble psychiatrique donnant lieu à des atteintes comportementales',
+      'Handicap mental : déficience limitant les activités d’une personne'
     ],
     label: 'Détails de certaines options'
   }
