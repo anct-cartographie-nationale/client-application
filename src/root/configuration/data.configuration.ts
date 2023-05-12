25886e5240a579d84a3c347424c5ab4d4fa19be8
Marc Gavanier <marc.gavanier@gmail.com>
2023-05-12 14:18:07 +0200

    feat: use data inclusion ressourcs without duplicates

--- src/root/configuration/data.configuration.ts
+++ src/root/configuration/data.configuration.ts
@@ -7,5 +7,5 @@ export type DataConfiguration = {
 export const DATA_TOKEN: InjectionToken<DataConfiguration> = new InjectionToken<DataConfiguration>('data.configuration');
 
 export const DATA_CONFIGURATION: DataConfiguration = {
-  lieuxDeMediationNumerique: 'https://www.data.gouv.fr/fr/datasets/r/b5e5a1e1-122e-4f87-b6cf-d1ce342671be'
+  lieuxDeMediationNumerique: 'https://www.data.gouv.fr/fr/datasets/r/bdfef095-f3a9-4e66-84bb-ea05d5c9c3a5'
 };
