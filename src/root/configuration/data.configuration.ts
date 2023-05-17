f960a6b50bca4f53076d8d64c18579a61c5a70da
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2023-05-17 10:24:33 +0200

    feat: hub du sud deploy url

--- src/root/configuration/data.configuration.ts
+++ src/root/configuration/data.configuration.ts
@@ -17,5 +17,6 @@ export const DATA_COORDINATEURS_CONFIGURATION: DataCoordinateursConfiguration = 
 export const DATA_TOKEN: InjectionToken<DataConfiguration> = new InjectionToken<DataConfiguration>('data.configuration');
 
 export const DATA_CONFIGURATION: DataConfiguration = {
-  lieuxDeMediationNumerique: '/assets/data/lieux-inclusion-numerique.json'
+  lieuxDeMediationNumerique:
+    '../../assets/data/20230516-hub-du-sud-lieux-de-mediation-numeriques-provence-alpes-cote-d-azur.json'
 };
