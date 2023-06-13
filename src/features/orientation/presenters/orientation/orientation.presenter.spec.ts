3b7a531e5825164bc8215fdb7c09e0c04f6a9d0c
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2023-06-13 17:35:42 +0200

    fix: test

--- src/features/orientation/presenters/orientation/orientation.presenter.spec.ts
+++ src/features/orientation/presenters/orientation/orientation.presenter.spec.ts
@@ -5,7 +5,7 @@ describe('orientation presenter', (): void => {
     const email: string = filterNotFoundEmailBody();
 
     expect(email).toStrictEqual(
-      "Bonjour 👋,%0D%0A%0D%0AJe ne trouve pas mon besoin dans la liste de vos besoins sur le parcours d'orientation de la cartographie nationale.%0D%0AJ'aimerai pouvoir filtrer les structures selon :%0D%0A%0D%0A%0D%0A%0D%0ANumériquement, à bientôt !"
+      "Bonjour 👋,%0D%0A%0D%0AJe ne trouve pas mon besoin dans la liste de vos besoins sur le parcours d'orientation de la cartographie nationale.%0D%0AJ'aimerai pouvoir filtrer les lieux selon :%0D%0A%0D%0A%0D%0A%0D%0ANumériquement, à bientôt !"
     );
   });
 });
