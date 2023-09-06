a325e27571a3bf9c3df6d2815142c9597eb7aa60
Marc Gavanier <marc.gavanier@gmail.com>
2023-09-06 09:44:32 +0200

    feat: add presentation link (#350)

--- src/features/core/components/index.ts
+++ src/features/core/components/index.ts
@@ -1,4 +1,5 @@
 export * from './collapse/collapse.module';
 export * from './modal/modal.module';
 export * from './modal/modal.component';
+export * from './offcanvas/offcanvas.component';
 export * from './offcanvas/offcanvas.module';
