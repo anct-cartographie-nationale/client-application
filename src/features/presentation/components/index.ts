e41478e67463b3284446f46b515a4ea28c06d211
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2023-09-13 17:18:10 +0200

    feat: add section integrateurs presentation page

--- src/features/presentation/components/index.ts
+++ src/features/presentation/components/index.ts
@@ -1,1 +1,2 @@
 export * from './carousel/carousel.component';
+export * from './integrateurs/integrateurs.component';
--- src/features/presentation/modules/presentation.feature.module.ts
+++ src/features/presentation/modules/presentation.feature.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SkipLinkModule } from '../../core';
-import { CarouselComponent } from '../components';
+import { CarouselComponent, IntegrateursComponent } from '../components';
 import { PresentationPage } from '../pages';
 import { PresentationFeatureRoutingModule } from './presentation.feature-routing.module';
 
 @NgModule({
-  declarations: [PresentationPage, CarouselComponent],
+  declarations: [PresentationPage, CarouselComponent, IntegrateursComponent],
   imports: [PresentationFeatureRoutingModule, CommonModule, SkipLinkModule]
 })
 export class PresentationFeatureModule {}
