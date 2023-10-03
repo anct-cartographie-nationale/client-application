b5c93a8a6d1c4a12302ea2e6dbc578c97fd4b4db
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2023-10-03 19:34:27 +0200

    feat: revamp services layout

--- src/features/cartographie/components/lieux-mediation-numerique-details/services/services.component.ts
+++ src/features/cartographie/components/lieux-mediation-numerique-details/services/services.component.ts
@@ -1,5 +1,11 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Service } from '@gouvfr-anct/lieux-de-mediation-numerique';
+import servicesMapping from './services-mapping.json';
+
+type ServicesMapping = {
+  icon: string;
+  value: string;
+};
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -8,4 +14,10 @@ import { Service } from '@gouvfr-anct/lieux-de-mediation-numerique';
 })
 export class ServicesComponent {
   @Input() public services?: Service[];
+
+  public iconFromServices: ServicesMapping[] = servicesMapping;
+
+  public getIconFromServices(service: string): string | undefined {
+    return this.iconFromServices.find((fromService: ServicesMapping) => fromService.value === service)?.icon;
+  }
 }
