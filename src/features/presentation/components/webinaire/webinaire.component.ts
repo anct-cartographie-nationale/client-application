8f3523181f3935814bd97575cfa47fb0f9a41ea0
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2023-10-03 13:37:05 +0200

    feat: add webinaire url

--- src/features/presentation/components/webinaire/webinaire.component.ts
+++ src/features/presentation/components/webinaire/webinaire.component.ts
@@ -19,21 +19,21 @@ export class WebinaireComponent {
       titre: 'Apprenez à orienter vos bénéficiaires grâce à la cartographie',
       duration: '45 min - 1 fois par mois',
       text: `Consacré à l'utilisation du parcours d'orientation et la navigation sur la carte. `,
-      inscription_link: '',
+      inscription_link: 'https://airtable.com/app2MF0d9XxkCwv0p/shrfwVic55IqzxKfG',
       image: 'webinaire-orientation.svg'
     },
     {
       titre: 'Mettez à jour vos données sur la cartographie',
       duration: '45 min - 1 fois par mois',
       text: `Consacré à l'utilisation des outils de mise à jour de données, en particulier l'outil DORA.`,
-      inscription_link: '',
+      inscription_link: 'https://airtable.com/app2MF0d9XxkCwv0p/shrX7zSTk8pOtoKik',
       image: 'webinaire-mise-a-jour.svg'
     },
     {
       titre: 'Permanence visio : posez vos questions et nous y répondons',
       duration: '45 min - 1 fois par mois',
       text: `Nous sommes à votre écoute pour répondre à vos questions et vos besoins spécifiques.`,
-      inscription_link: '',
+      inscription_link: 'https://airtable.com/app2MF0d9XxkCwv0p/shriflITshG3ba1RC',
       image: 'webinaire-portes-ouvertes.svg'
     }
   ];
