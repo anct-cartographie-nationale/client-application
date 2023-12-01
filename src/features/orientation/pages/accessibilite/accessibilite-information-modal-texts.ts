e0d2fbfaaef0d8416bc2a8c02471ffa45841a379
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2023-12-01 12:04:17 +0100

    fix: add title to all links for accessibility

--- src/features/orientation/pages/accessibilite/accessibilite-information-modal-texts.ts
+++ src/features/orientation/pages/accessibilite/accessibilite-information-modal-texts.ts
@@ -23,7 +23,7 @@ export const ACCESSIBILITE_INFORMATION_MODAL_TEXTS: Record<
   },
   'accueil-specifique': {
     titre: 'Accueil adapté',
-    description: `Si vous sélectionnez certaines de ces options, vous restreindrez votre recherche aux lieux en capacité d'accueillir certains types de publics. Pour toute information concernant les déplacements des personnes en situation de handicap, rendez-vous sur <a href="https://acceslibre.beta.gouv.fr/" target="_blank" rel="noopener noreferrer">Accès libre</a>`,
+    description: `Si vous sélectionnez certaines de ces options, vous restreindrez votre recherche aux lieux en capacité d'accueillir certains types de publics. Pour toute information concernant les déplacements des personnes en situation de handicap, rendez-vous sur <a href="https://acceslibre.beta.gouv.fr/" target="_blank" title="Accès libre (nouvel onglet)" rel="noopener noreferrer">Accès libre</a>`,
     elements: [
       'Handicap psychique : trouble psychiatrique donnant lieu à des atteintes comportementales',
       'Handicap mental : déficience limitant les activités d’une personne'
