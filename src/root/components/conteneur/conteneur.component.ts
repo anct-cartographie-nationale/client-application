835d3e50ae8e81022ed40d57a453e74a19e880ee
Arthur Belkhayat <arthur.belkhayat@gmail.com>
2023-12-01 12:44:07 +0100

    feat: scroll to top on navigate (#399)

--- src/root/components/conteneur/conteneur.component.ts
+++ src/root/components/conteneur/conteneur.component.ts
@@ -1,13 +1,15 @@
-import { ChangeDetectionStrategy, Component, ElementRef, Inject, Input, Renderer2, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Inject, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { OffcanvasComponent, SkipLinkPresenter } from '../../../features/core';
 import { BRAND_CONFIGURATION, BRAND_TOKEN, BrandConfiguration } from '../../configuration';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-conteneur',
   templateUrl: './conteneur.component.html'
 })
-export class ConteneurComponent {
+export class ConteneurComponent implements OnInit {
   @Input() set logo(logo: string) {
     BRAND_CONFIGURATION.logo = logo;
   }
@@ -18,12 +20,23 @@ export class ConteneurComponent {
 
   @ViewChild('navbarOffcanvas') navbarOffcanvas!: OffcanvasComponent;
 
+  @ViewChild('container', { static: true }) container!: ElementRef;
+
   public constructor(
     @Inject(BRAND_TOKEN) public readonly brandConfiguration: BrandConfiguration,
     public readonly skipLinkPresenter: SkipLinkPresenter,
     private renderer: Renderer2,
-    private elementRef: ElementRef
+    private elementRef: ElementRef,
+    public readonly router: Router
   ) {}
+
+  ngOnInit() {
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+      if (this.container.nativeElement) {
+        this.container.nativeElement.scrollTop = 0;
+      }
+    });
+  }
 
   public skipTo(id: string): void {
     const targetElement = this.elementRef.nativeElement.querySelector(`#${id}`);
