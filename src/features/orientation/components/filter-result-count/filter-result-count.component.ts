7544bc20ed4c4c521c3c3c9f19335d4712354fee
Arthur Belkhayat <arthur.belkhayat@gmail.com>
2023-12-19 17:14:10 +0100

    fix: text lisible zoom a 200% widget orientation (#407)

--- src/features/orientation/components/filter-result-count/filter-result-count.component.ts
+++ src/features/orientation/components/filter-result-count/filter-result-count.component.ts
@@ -1,12 +1,33 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-filter-result-count',
   templateUrl: './filter-result-count.component.html'
 })
-export class FilterResultCountComponent {
+export class FilterResultCountComponent implements AfterViewInit {
   @Input() public total: number = 0;
 
   @Input() public found: number = 0;
+
+  @ViewChild('textElement') public textElement!: ElementRef;
+
+  public initialFontSize: string = '32px';
+  public pageIsZoomed: boolean = false;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    this.pageIsZoomed = getComputedStyle(this.textElement.nativeElement).fontSize === this.initialFontSize ? false : true;
+    this.cdr.detectChanges();
+  }
 }
