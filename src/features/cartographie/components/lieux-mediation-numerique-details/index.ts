919370910fcc042352c8afc2183a03085391a6d1
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2024-01-18 14:24:19 +0100

    feat: new layout for source update details page

--- src/features/cartographie/components/lieux-mediation-numerique-details/index.ts
+++ src/features/cartographie/components/lieux-mediation-numerique-details/index.ts
@@ -14,6 +14,5 @@ export * from './lieux-mediation-numerique-details-print.component';
 export * from './accessibilite/accessibilite.component';
 export * from './erreur-form-modal/erreur-form-modal.component';
 export * from './impression-choice-modal/impression-choice-modal.component';
-export * from './source-donnees/source-donnees.component';
 export * from './webinaire/webinaire.component';
 export * from './source-footer/source-footer.component';
