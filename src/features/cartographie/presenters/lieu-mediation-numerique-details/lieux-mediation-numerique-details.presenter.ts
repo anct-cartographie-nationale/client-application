93902f3b2d9532b2a04d0838408be121db280e96
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2024-02-12 12:58:16 +0100

    fix: identifier double underscore

--- src/features/cartographie/presenters/lieu-mediation-numerique-details/lieux-mediation-numerique-details.presenter.ts
+++ src/features/cartographie/presenters/lieu-mediation-numerique-details/lieux-mediation-numerique-details.presenter.ts
@@ -495,7 +495,7 @@ const ifAnyHorairesWithWeeks =
     horaires?.includes('week') ? ifAny('full_horaires', getHorairesWeeksByWeeks(date)(horaires)) : [];
 
 const getMultipleSourcesIfAny = (id: string, lieuSource?: string): SourcePresentation[] =>
-  id.includes('_')
+  id.includes('__')
     ? Array.from(availableSourcesMap)
         .filter(([source]: [string, SourcePresentation]) => id.replace(/-/g, ' ').includes(source))
         .map(([, source]) => source)
