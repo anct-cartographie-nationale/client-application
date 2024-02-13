763c6bfeba90954aa6679ad59356abf052c10c99
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2024-02-13 13:17:34 +0100

    feat: le havre seine metropole cartographie

--- src/root/configuration/brand.configuration.ts
+++ src/root/configuration/brand.configuration.ts
@@ -11,15 +11,15 @@ export type BrandConfiguration = {
 export const BRAND_TOKEN: InjectionToken<BrandConfiguration> = new InjectionToken<BrandConfiguration>('brand.configuration');
 
 export const BRAND_CONFIGURATION: BrandConfiguration = {
-  name: 'Nationale',
+  name: 'Le Havre Seine Métropole',
   logo: '',
-  illustration: 'svg-france-et-outremer.svg'
+  illustration: 'svg-normandie.svg'
 };
 
 export const BRAND_APPLICATION_CONFIGURATION: BrandConfiguration = {
-  name: 'Nationale',
+  name: 'Le Havre Seine Métropole',
   sousTitre: "des lieux d'inclusion numérique",
   logo: './assets/img/logo/cartographie-nationale-brand.png',
   logoAvecTexte: './assets/img/logo/cartographie-nationale-brand-text.png',
-  illustration: 'svg-france-et-outremer.svg'
+  illustration: 'svg-normandie.svg'
 };
