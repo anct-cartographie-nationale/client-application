f754a5cd39a33daba92cb30ccbcaf0c949878928
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2024-02-27 09:38:18 +0100

    feat: add score completion on fiche details

--- src/features/cartographie/components/lieux-mediation-numerique-details/score-completion/score-completion.component.ts
+++ src/features/cartographie/components/lieux-mediation-numerique-details/score-completion/score-completion.component.ts
@@ -11,4 +11,17 @@ export class ScoreCompletionComponent {
   @Input() public lieuMediationNumerique!: LieuMediationNumeriqueDetailsPresentation;
 
   public scoreCompletion = scoreCompletion;
+
+  public scoreExplanation(score: number): string {
+    switch (true) {
+      case score >= 0 && score < 60:
+        return "Vous travaillez ici ? Nous vous conseillons d'enrichir les informations que nous possédont pour améliorer la visibilité de votre lieu";
+      case score >= 60 && score < 85:
+        return "C'est déjà bien ! Si vous travaillez ici, vous pouvez enrichir les informations que nous possédons";
+      case score >= 85:
+        return "Cette structure semble à jour avec la bonne quantité d'informations renseignée !";
+      default:
+        return '';
+    }
+  }
 }
