30a0866e78d4e3c9c82f3c6f983308683e5ab057
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2024-02-29 14:52:36 +0100

    fix: retour review

--- src/features/cartographie/components/lieux-mediation-numerique-details/score-completion/score-completion.component.ts
+++ src/features/cartographie/components/lieux-mediation-numerique-details/score-completion/score-completion.component.ts
@@ -13,15 +13,10 @@ export class ScoreCompletionComponent {
   public scoreCompletion = scoreCompletion;
 
   public scoreExplanation(score: number): string {
-    switch (true) {
-      case score >= 0 && score < 60:
-        return "Vous travaillez ici ? Nous vous conseillons d'enrichir les informations que nous possédont pour améliorer la visibilité de votre lieu";
-      case score >= 60 && score < 85:
-        return "C'est déjà bien ! Si vous travaillez ici, vous pouvez enrichir les informations que nous possédons";
-      case score >= 85:
-        return "Cette structure semble à jour avec la bonne quantité d'informations renseignée !";
-      default:
-        return '';
-    }
+    if (score >= 0 && score < 60)
+      return "Vous travaillez ici ? Nous vous conseillons d'enrichir les informations que nous possédont pour améliorer la visibilité de votre lieu";
+    else if (score >= 60 && score < 85)
+      return "C'est déjà bien ! Si vous travaillez ici, vous pouvez enrichir les informations que nous possédons";
+    else return "Cette structure semble à jour avec la bonne quantité d'informations renseignée !";
   }
 }
