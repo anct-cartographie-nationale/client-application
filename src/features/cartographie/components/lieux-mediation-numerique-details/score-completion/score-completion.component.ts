ec0f94ee608e0c8c94a88d0d2defbb7b04cf036f
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2024-03-05 13:19:08 +0100

    fix: same explanation for every score

--- src/features/cartographie/components/lieux-mediation-numerique-details/score-completion/score-completion.component.ts
+++ src/features/cartographie/components/lieux-mediation-numerique-details/score-completion/score-completion.component.ts
@@ -11,12 +11,4 @@ export class ScoreCompletionComponent {
   @Input() public lieuMediationNumerique!: LieuMediationNumeriqueDetailsPresentation;
 
   public scoreCompletion = scoreCompletion;
-
-  public scoreExplanation(score: number): string {
-    if (score >= 0 && score < 60)
-      return "Vous travaillez ici ? Nous vous conseillons d'enrichir les informations que nous possédont pour améliorer la visibilité de votre lieu";
-    else if (score >= 60 && score < 85)
-      return "C'est déjà bien ! Si vous travaillez ici, vous pouvez enrichir les informations que nous possédons";
-    else return "Cette structure semble à jour avec la bonne quantité d'informations renseignée !";
-  }
 }
