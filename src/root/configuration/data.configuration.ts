9c6653eedcb74dc06ff459462fada9fac028b14a
Arthur Belkhayat <arthur.belkhayat@beta.gouv.fr>
2024-03-20 15:57:23 +0100

    feat: nouvelle caledonie carto locale

--- src/root/configuration/data.configuration.ts
+++ src/root/configuration/data.configuration.ts
@@ -17,5 +17,5 @@ export const DATA_COORDINATEURS_CONFIGURATION: DataCoordinateursConfiguration = 
 export const DATA_TOKEN: InjectionToken<DataConfiguration> = new InjectionToken<DataConfiguration>('data.configuration');
 
 export const DATA_CONFIGURATION: DataConfiguration = {
-  lieuxDeMediationNumerique: '/assets/data/lieux-inclusion-numerique.json'
+  lieuxDeMediationNumerique: './assets/data/20240320-nouvelle-caledonie-lieux-de-mediation-numeriques-nouvelle-caledonie.json'
 };
