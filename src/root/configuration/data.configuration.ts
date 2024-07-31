b370251eeba6621247212039d8e1725e59cd4967
Marc Gavanier <marc.gavanier@gmail.com>
2024-07-31 11:02:53 +0200

    fix: use self hosted data instead of data.gouv.fr (#515)

--- src/root/configuration/data.configuration.ts
+++ src/root/configuration/data.configuration.ts
@@ -17,5 +17,5 @@ export const DATA_COORDINATEURS_CONFIGURATION: DataCoordinateursConfiguration = 
 export const DATA_TOKEN: InjectionToken<DataConfiguration> = new InjectionToken<DataConfiguration>('data.configuration');
 
 export const DATA_CONFIGURATION: DataConfiguration = {
-  lieuxDeMediationNumerique: 'https://www.data.gouv.fr/fr/datasets/r/b5e5a1e1-122e-4f87-b6cf-d1ce342671be'
+  lieuxDeMediationNumerique: '/assets/data/lieux-inclusion-numerique.json'
 };
