596933c2779d84d936405f8d67efb670d244930d
Marc Gavanier <marc.gavanier@protonmail.com>
2025-02-06 16:30:42 +0100

    feat: update logo

--- src/root/configuration/brand.configuration.ts
+++ src/root/configuration/brand.configuration.ts
@@ -19,7 +19,7 @@ export const BRAND_CONFIGURATION: BrandConfiguration = {
 export const BRAND_APPLICATION_CONFIGURATION: BrandConfiguration = {
   name: 'Aix-en-Provence',
   sousTitre: "des lieux d'inclusion numérique",
-  logo: './assets/img/logo/aix-en-provence.jpg',
+  logo: './assets/img/logo/aix-en-provence.png',
   logoAvecTexte: '',
   illustration: 'svg-paca.svg'
 };
